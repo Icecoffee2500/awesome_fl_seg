f4aa84d8e1bbde675c784beacaabc57cb7e31c92
qwest <icecoffee2500@gmail.com>
2025-10-14 21:03:04 +0900

    Resize train shell modified

--- Resize_trains_3.sh
+++ Resize_trains_3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # CL
-uv run -m train_cl 'dataset.crop_size=[448,448]' device_id=2
+uv run -m train_cl 'dataset.crop_size=[448,448]' device_id=3
 
 # FL
 uv run -m train_fl -- 'fl.target_resolutions={0:[768,768],1:[768,768],2:[768,768]}' device_id=3

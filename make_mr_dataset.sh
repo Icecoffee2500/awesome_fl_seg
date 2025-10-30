14cd652a36ae3e36a65eff6a819b992a55f1b150
qwest <icecoffee2500@gmail.com>
2025-10-30 16:58:16 +0900

    make_mr_dataset.sh modified

--- make_mr_dataset.sh
+++ make_mr_dataset.sh
@@ -5,4 +5,4 @@ uv run -m create_mr_cityscapes 640 320
 uv run -m create_mr_cityscapes 768 384
 uv run -m create_mr_cityscapes 896 448
 
-uv run -m create_mr_cityscapes 1024 512
+# uv run -m create_mr_cityscapes 1024 512

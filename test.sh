693ab92ef4e57b8ca3f15e0f985126d9ccea252e
qwest <icecoffee2500@gmail.com>
2025-10-31 13:59:11 +0900

    test.sh modified

--- test.sh
+++ test.sh
@@ -6,22 +6,30 @@
 # # Federated Learning
 # uv run -m test_cl 'test_checkpoint_path={checkpoint 폴더}/best_model_fl.pth' 'test_data_root={테스트할 해상도의 데이터셋 위치}' 'dataset.test_scale={테스트할 데이터셋의 해상도}'
 
-# MRKD Federated Learning
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes' 'dataset.test_scale=[2048, 1024]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_1536x768' 'dataset.test_scale=[1536, 768]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_1024x512' 'dataset.test_scale=[1024, 512]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_896x448' 'dataset.test_scale=[896, 448]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_768x384' 'dataset.test_scale=[768, 384]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_640x320' 'dataset.test_scale=[640, 320]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_512x256' 'dataset.test_scale=[512, 256]' device_id=6
+uv run -m test_cl 'test_checkpoint_path=performance/1030-2111/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes' 'dataset.test_scale=[2048, 1024]' device_id=6
+uv run -m test_cl 'test_checkpoint_path=performance/1030-2111/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_1536x768' 'dataset.test_scale=[1536, 768]' device_id=6
+uv run -m test_cl 'test_checkpoint_path=performance/1030-2111/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_1024x512' 'dataset.test_scale=[1024, 512]' device_id=6
+uv run -m test_cl 'test_checkpoint_path=performance/1030-2111/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_896x448' 'dataset.test_scale=[896, 448]' device_id=6
+uv run -m test_cl 'test_checkpoint_path=performance/1030-2111/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_768x384' 'dataset.test_scale=[768, 384]' device_id=6
+uv run -m test_cl 'test_checkpoint_path=performance/1030-2111/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_640x320' 'dataset.test_scale=[640, 320]' device_id=6
+uv run -m test_cl 'test_checkpoint_path=performance/1030-2111/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_512x256' 'dataset.test_scale=[512, 256]' device_id=6
 
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes' 'dataset.test_scale=[2048, 1024]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_1536x768' 'dataset.test_scale=[1536, 768]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_1024x512' 'dataset.test_scale=[1024, 512]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_896x448' 'dataset.test_scale=[896, 448]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_768x384' 'dataset.test_scale=[768, 384]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_640x320' 'dataset.test_scale=[640, 320]' device_id=6
-uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_512x256' 'dataset.test_scale=[512, 256]' device_id=6
+# # MRKD Federated Learning
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes' 'dataset.test_scale=[2048, 1024]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_1536x768' 'dataset.test_scale=[1536, 768]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_1024x512' 'dataset.test_scale=[1024, 512]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_896x448' 'dataset.test_scale=[896, 448]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_768x384' 'dataset.test_scale=[768, 384]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_640x320' 'dataset.test_scale=[640, 320]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1439/fl_512x512_384x384_256x256_gpu6_best_model.pth' 'test_data_root=data/cityscapes_512x256' 'dataset.test_scale=[512, 256]' device_id=6
+
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes' 'dataset.test_scale=[2048, 1024]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_1536x768' 'dataset.test_scale=[1536, 768]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_1024x512' 'dataset.test_scale=[1024, 512]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_896x448' 'dataset.test_scale=[896, 448]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_768x384' 'dataset.test_scale=[768, 384]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_640x320' 'dataset.test_scale=[640, 320]' device_id=6
+# uv run -m test_cl 'test_checkpoint_path=performance/1029-1441/fl_1024x1024_768x768_512x512_gpu7_best_model.pth' 'test_data_root=data/cityscapes_512x256' 'dataset.test_scale=[512, 256]' device_id=6
 
 # # Clustered Federated Learning
 # uv run -m test_cl 'test_checkpoint_path=performance/1028-1721/fl_no_RandomResize_no_RandomCropgpu6_best_model.pth' 'test_data_root=data/cityscapes' 'dataset.test_scale=[2048, 1024]' device_id=6

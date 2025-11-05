32c3d8c625136dd46eeb5b89f73ed0e9d43902d7
qwest <icecoffee2500@gmail.com>
2025-11-05 17:25:11 +0900

    train_shell modified

--- mrkd_fl_trains_6.sh
+++ mrkd_fl_trains_6.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-uv run -m train_fl device_id=6
-uv run -m train_fl -- 'fl.target_resolutions={0:[1024,1024],1:[1024,1024],2:[1024,1024]}' device_id=6
+# uv run -m train_fl device_id=6
+# uv run -m train_fl -- 'fl.target_resolutions={0:[1024,1024],1:[1024,1024],2:[1024,1024]}' device_id=6
+uv run -m train_fl -- 'fl.target_resolutions={0:[768,768],1:[768,768],2:[768,768]}' device_id=6
+uv run -m train_fl -- 'fl.target_resolutions={0:[448,448],1:[448,448],2:[448,448]}' device_id=6
--- mrkd_fl_trains_7.sh
+++ mrkd_fl_trains_7.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-uv run -m train_fl -- 'fl.target_resolutions={0:[1024,1024],1:[768,768],2:[512,512]}' device_id=7
-uv run -m train_fl -- 'fl.target_resolutions={0:[512,512],1:[512,512],2:[512,512]}' device_id=7
+# uv run -m train_fl -- 'fl.target_resolutions={0:[1024,1024],1:[768,768],2:[512,512]}' device_id=7
+# uv run -m train_fl -- 'fl.target_resolutions={0:[512,512],1:[512,512],2:[512,512]}' device_id=7
+uv run -m train_fl -- 'fl.target_resolutions={0:[384,384],1:[384,384],2:[384,384]}' device_id=7
+uv run -m train_fl -- 'fl.target_resolutions={0:[320,320],1:[320,320],2:[320,320]}' device_id=7
+uv run -m train_fl -- 'fl.target_resolutions={0:[256,256],1:[256,256],2:[256,256]}' device_id=7
